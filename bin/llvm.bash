6a80f4de1e59a77849008f791fa1240465d4fde3
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-02 07:46:00 -0500

    work

--- bin/llvm.bash
+++ bin/llvm.bash
@@ -1,5 +1,17 @@
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" ../llvm
+cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang" ../llvm
+# -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra'
+    # semicolon-separated list of the LLVM subprojects you'd like to additionally build. clangd is in clang-tools-extra.
+# -DCMAKE_INSTALL_PREFIX=/usr/local  # LLVM tools and libraries to be installed (default /usr/local).
+# -DCMAKE_BUILD_TYPE=Debug  # Debug, Release, RelWithDebInfo, and MinSizeRel. Default is Debug.
+
+# Linking CXX executable bin/clang
+# collect2: fatal error: ld terminated with signal 9 [Killed]
+# This may be caused by the linking step using too much memory OOM. 
+# 1, -DLLVM_PARALLEL_LINK_JOBS=1    # not work
+# 2, -DLLVM_LINK_LLVM_DYLIB=true    # need re-compile, not good
+# 3, -DCMAKE_BUILD_TYPE=Release     # need re-compile, not good
+# 3, -DLLVM_USE_LINKER=gold         # need re-link
 make 
 make install
