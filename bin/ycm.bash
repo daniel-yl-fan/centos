e5251fe8deb04a759283300b894f82a390db37ed
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-02 12:37:03 -0500

    work

--- bin/ycm.bash
+++ bin/ycm.bash
@@ -7,3 +7,4 @@ yum install python36
 yum install python36-devel
 cmake -G "Unix Makefiles" -DUSE_PYTHON2=OFF -DEXTERNAL_LIBCLANG_PATH=/home/llvm-project/build/lib/libclang.so . /usr/share/vim/vim80/plugged/YouCompleteMe/third_party/ycmd/cpp
 make
+ln -s  /home/work/cnfg/.ycm_extra_conf.py /usr/share/vim/vim80/plugged/YouCompleteMe/
