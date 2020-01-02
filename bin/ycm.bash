9e3272e7c71b23160dcdb5f0d8db6f22509d5332
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-02 11:58:20 -0500

    work

---
+++ bin/ycm.bash
@@ -0,0 +1,9 @@
+mkdir /tmp/ycm-build
+cd /tmp/ycm-build
+# default compile using python2
+cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=/home/llvm-project/build/lib/libclang.so.10 . /usr/share/vim/vim80/plugged/YouCompleteMe/third_party/ycmd/cpp
+# using python3
+yum install python36
+yum install python36-devel
+cmake -G "Unix Makefiles" -DUSE_PYTHON2=OFF -DEXTERNAL_LIBCLANG_PATH=/home/llvm-project/build/lib/libclang.so . /usr/share/vim/vim80/plugged/YouCompleteMe/third_party/ycmd/cpp
+make
