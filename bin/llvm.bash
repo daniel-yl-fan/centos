b8ac998af3e45d02ce9dfccee44b99177687d961
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-06 08:51:12 -0500

    work

--- bin/llvm.bash
+++ bin/llvm.bash
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_COMPILE_JOBS=1 -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_USE_LINKER=gold -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_APPEND_VC_REV=OFF -DLLVM_ENABLE_UNWIND_TABLES=OFF ../llvm
+cmake -G "Unix Makefiles" -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DCMAKE_BUILD_TYPE=Release -DLLVM_PARALLEL_COMPILE_JOBS=1 -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_ENABLE_LLD=ON -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_APPEND_VC_REV=OFF -DLLVM_ENABLE_UNWIND_TABLES=OFF ../llvm
 # -DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra'
     # semicolon-separated list of the LLVM subprojects you'd like to additionally build. clangd is in clang-tools-extra.
 # -DCMAKE_INSTALL_PREFIX=/usr/local  # LLVM tools and libraries to be installed (default /usr/local).
@@ -12,6 +12,7 @@ cmake -G "Unix Makefiles" -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS="cl
 # 1, -DLLVM_PARALLEL_LINK_JOBS=1
 # 2, -DCMAKE_BUILD_TYPE=Release
 # 3, -DLLVM_USE_LINKER=gold
+# 4, -DLLVM_ENABLE_LLD=ON  # using lld much faster than bfd/gold
 
 cmake --build . --target install
 
