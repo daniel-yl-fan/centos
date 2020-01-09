c88d5c2fda9f8180fc77ee6be93fb5bfb45187c7
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-09 06:48:10 -0500

    centos

--- bin/centos_setup.bash
+++ bin/centos_setup.bash
@@ -1,3 +1,11 @@
+git clone git@github.com:daniel-yl-fan/centos.git
+
+# todo
+
+ln -s /home/centos/config/.tmux.conf
+ln -s /home/centos/config/.vimrc
+ln -s /home/centos/config/.gitconfig
+
 yum --assumeyes install tmux
 yum --assumeyes install python2-devel
 yum --assumeyes install python36
@@ -7,8 +15,10 @@ yum --assumeyes install https://download-ib01.fedoraproject.org/pub/epel/7/x86_6
 yum --assumeyes install https://download-ib01.fedoraproject.org/pub/fedora/linux/updates/29/Everything/x86_64/Packages/f/fzf-0.18.0-1.fc29.x86_64.rpm
 
 ####  z.sh
+cd /tmp
 git clone https://github.com/rupa/z.git /tmp/z
 mv --force /tmp/z/z.sh /home/bin/z.sh
+chmod 444 /home/bin/z.sh
 rm --force --recursive /tmp/z
 # in .bashrc    source /home/bin/z.sh
 
@@ -16,6 +26,22 @@ rm --force --recursive /tmp/z
 cd /tmp
 wget https://github.com/jingweno/ccat/releases/download/v1.1.0/linux-amd64-1.1.0.tar.gz
 tar -xzvf linux-amd64-1.1.0.tar.gz
-mv --force /tmp/linux-amd64-1.1.0/ccat /home/bin/cat
+mv --force /tmp/linux-amd64-1.1.0/ccat /home/bin/ccat
+chmod 500 /home/bin/ccat
 rm --force --recursive /tmp/linux-amd64-1.1.0
 rm --force --recursive /tmp/linux-amd64-1.1.0.tar.gz
+
+####  cheatsheets
+cd /tmp
+wget https://github.com/cheat/cheat/releases/download/3.2.1/cheat-linux-amd64
+mv /tmp/cheat-linux-amd64 /home/bin/cheat
+chmod 500 /home/bin/cheat
+cd /root/cheat
+cheat --init > /root/cheat/config.yaml
+
+cd /tmp
+git clone https://github.com/cheat/cheatsheets.git /root/cheat/community
+sed --in-place --expression="s/.dotfiles\///" /root/cheat/config.yaml
+sed --in-place --expression="\$aexport CHEAT_CONFIG_PATH="/root/cheat/config.yaml"" /root/.bashrc
+# git clone https://github.com/cheat/cheatsheets.git /home/cheatsheets/community
+# git clone https://github.com/skywind3000/awesome-cheatsheets.git /home/cheatsheets/skywind3000
---
+++ bin/cheatsheets.bash
@@ -0,0 +1,2 @@
+git clone https://github.com/cheat/cheatsheets.git /home/cheatsheets/community
+git clone https://github.com/skywind3000/awesome-cheatsheets.git /home/cheatsheets/skywind3000
