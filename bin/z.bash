c676d78e51c079ea56ec28158a0f1ef224c80288
daniel-yl-fan <daniel_qdcn@hotmail.com>
2020-01-09 03:58:43 -0500

    centos

--- bin/z.bash
+++ bin/z.bash
@@ -1,4 +1,3 @@
 git clone https://github.com/rupa/z.git /tmp/z
-mv --force /tmp/z/z.sh /usr/bin/z.sh
-mv --force /tmp/z/z.1 /usr/share/man/man1/z.1
-sed --in-place --expression="\$asource /usr/bin/z.sh" /root/.bashrc
+mv --force /tmp/z/z.sh /home/bin/z.sh
+rm --force --recursive /tmp/z
