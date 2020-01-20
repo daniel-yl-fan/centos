297b0aceb6be7f1c0f9b4d57687869962dd586b4
daniel-yl-fan <59277439+daniel-yl-fan@users.noreply.github.com>
2020-01-21 01:18:30 +0800

    centos

--- bin/centos_setup.bash
+++ bin/centos_setup.bash
@@ -5,7 +5,6 @@ yum install perl gcc dkms kernel-devel kernel-headers make bzip2
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 
-
 # ssh-keygen, ~/.ssh/id_rsa.pub github.com/daniel-yl-fan
 
 mkdir -p ~/local
